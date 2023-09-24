7376813509f65f7eaaeed81af44d60145fa7093d
AlphA Diallo <125399583+alphadl0@users.noreply.github.com>
2023-09-24 09:11:31 +0000

    new

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Mamadou Alpha Diallo",
+  title: "Cyber Security Analyst",
+  email: "mamadoualpha-diallo@outlook.com",
+  gitHub: "alphadl0",
+  instagram: "alpha_dl0",
+  linkedIn: "alpha-dl0",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "alphadl0",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,21 +10,21 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/safety.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = ".";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a 2nd year Management Information Systems (MIS) student at Dokuz Eylül University, focusing on cybersecurity. I love learning and applying new technologies to solve real-world problems";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  " I am developing my expertise in cybersecurity through rigorous coursework, hands-on projects, and a commitment to staying up-to-date with the latest industry trends and best practices. My journey in cybersecurity has equipped me with a diverse set of skills, ranging from network security and ethical hacking to data privacy and risk management. In this portfolio, you will discover a collection of my most significant cybersecurity projects and accomplishments.";
 
 const About = () => {
   return (
